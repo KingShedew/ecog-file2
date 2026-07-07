6dda5eda5527e50fe051db81e24c52f42aa02ca2
MTKSIKM <kingshedew@gmail.com>
2026-07-07 15:57:13 -0500

    Modify window.open calls to use '_self' target
    Updated window.open calls to use '_self' target for navigation.

---
+++ prodigy/chatgptsucks.js
@@ -0,0 +1,12 @@
+Find this:
+window.open("../" + version + "/");
+Replace it with:
+window.open("../" + version + "/", "_self");
+Find this:
+window.open("../" + version + "/" + ModString)
+Replace it with:
+window.open("../" + version + "/" + ModString, "_self")
+Find this:
+window.open("https://oldprodigy.onrender.com/play/");
+Replace it with:
+window.open("https://oldprodigy.onrender.com/play/", "_self");
